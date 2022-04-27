a6484abc642befe691c721efe96ae52d0d6e7717
andydangit <andy.dang.it@gmail.com>
2022-04-27 14:27:04 -0500

    Refactor: update movie props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
-import "./App.css";
+import MovieCard from "./components/MovieCard";
 import SearchIcon from "./Search.svg";
+
+import "./App.css";
+
 
 function App() {
   const API_URL = "http://www.omdbapi.com?apikey=b8d1252e";
@@ -40,25 +43,7 @@ function App() {
         <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
       <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-          <div>
-            <img
-              src={
-                movie1.Poster !== "N/A"
-                  ? movie1.Poster
-                  : "https://via.placeholder.com/400"
-              }
-              alt={movie1.Title}
-            />
-          </div>
-          <div>
-            <span> {movie1.Type} </span>
-            <h3> {movie1.Title}</h3>
-          </div>
-        </div>
+<MovieCard movie1={movie1}/>
       </div>
     </div>
   );
